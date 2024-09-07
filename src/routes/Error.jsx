0263e1b56b288f281da297e62b8a0c750f1ed8a2
Renan <renandiutida@gmail.com>
2024-09-07 04:20:39 -0300

    Ajustando alguns detalhes de algumas paginas e do error - Gustavo Melanda - RM 556081

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,10 +1,12 @@
 import '../css/error.css'
+import EmojiError from '../assets/error-404.png'
 
 const Error=()=>{
 
     return(
         <main className="error">
-            <h2>Error 404 - Page Not Found!</h2>
+            <h1>Error 404 - Page Not Found!</h1>
+            <img src={EmojiError} alt="error-404" />
         </main>
     )
 }
